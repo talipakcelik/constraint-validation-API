3394e4edb01c63bb5e047c6af0a52f620fa6a0a8
Talip <talipakcelik@hacettepe.edu.tr>
2022-02-12 23:22:22 +0300

    Add missingValue validation for password and confirm password

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const emailErrMsg = document.querySelector('.errMessage');
 const country = document.querySelector('#country');
 const zip = document.querySelector('#zip');
 const zipErrMsg = document.querySelector('.zipErrMsg');
+const pw = document.querySelector('#pw');
+const pwErrMsg = document.querySelector('.pwErrMsg');
+const confirmPw = document.querySelector('#confirm-pw');
+const confirmPwErrMsg = document.querySelector('.confirmPwErrMsg');
 
 console.log(email.validity);
 
@@ -38,25 +42,7 @@ email.addEventListener('invalid', function () {
   emailErrMsg.textContent = 'Entered value needs to be a gmail address.';
 });
 
-function showError() {
-  if (email.validity.valueMissing) {
-    // If the field is empty, display the following error message.
-
-    emailErrMsg.textContent = 'You need to enter an e-mail address.';
-  } else if (email.validity.typeMismatch) {
-    // If the field doesn't contain an email address, display the following error message.
-
-    emailErrMsg.textContent = 'Entered value needs to be a gmail address.';
-  }
-
-  if (zip.validity.valueMissing) {
-    zipErrMsg.textContent = 'You need to enter a zip code.';
-  }
-}
-console.log(zip.validity);
 zip.addEventListener('change', function () {
-  let zipCheck = zip.checkValidity();
-
   if (zip.validity.valueMissing) {
     zipErrMsg.textContent = 'You need to enter a zip code.';
     zip.setCustomValidity('Please enter a zip code');
@@ -71,6 +57,54 @@ zip.addEventListener('invalid', function () {
   zipErrMsg.textContent = 'Entered value needs to be a valid zip code.';
 });
 
+pw.addEventListener('change', function () {
+  if (pw.validity.valueMissing) {
+    pwErrMsg.textContent = 'You need to enter a password.';
+    pw.setCustomValidity('Please enter a password');
+    pw.reportValidity();
+  } else {
+    pw.setCustomValidity('');
+    pwErrMsg.textContent = '';
+  }
+});
+
+confirmPw.addEventListener('change', function () {
+  if (confirmPw.validity.valueMissing) {
+    confirmPwErrMsg.textContent = 'You need to enter a confirm password.';
+    confirmPw.setCustomValidity('Please enter a confirm password');
+    confirmPw.reportValidity();
+  } else {
+    confirmPw.setCustomValidity('');
+    confirmPwErrMsg.textContent = '';
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    // If the field is empty, display the following error message.
+
+    emailErrMsg.textContent = 'You need to enter an e-mail address.';
+  } else if (email.validity.typeMismatch) {
+    // If the field doesn't contain an email address, display the following error message.
+
+    emailErrMsg.textContent = 'Entered value needs to be a gmail address.';
+  }
+
+  if (zip.validity.valueMissing) {
+    zipErrMsg.textContent = 'You need to enter a zip code.';
+  }
+
+  if (pw.validity.valueMissing) {
+    pwErrMsg.textContent = 'You need to enter a password';
+  } else if (pw.validity.typeMismatch) {
+    pwErrMsg.textContent = 'Entered value needs to be a password';
+  }
+
+  if (pw.validity.valueMissing) {
+    pwErrMsg.textContent = 'You need to enter a password';
+  }
+}
+
 form.addEventListener('submit', function (event) {
   let emailCheck = email.checkValidity();
 
@@ -81,6 +115,20 @@ form.addEventListener('submit', function (event) {
 
   let zipCheck = zip.checkValidity();
   if (!zipCheck) {
+    showError();
+    event.preventDefault();
+  }
+
+  let pwCheck = pw.checkValidity();
+
+  if (!pwCheck) {
+    showError();
+    event.preventDefault();
+  }
+
+  let confirmPwCheck = confirmPw.checkValidity();
+
+  if (!confirmPwCheck) {
     showError();
     event.preventDefault();
   }
